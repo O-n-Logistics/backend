bf0ba793bf364c3243f7c2064b2901d44d419e87
jeonjuho <wjswngh23@daum.net>
2025-04-01 22:42:37 +0900

    fix: 다중 상품 재고감소 요청 dto 수정

--- order-service/src/main/java/on/logistics/orderservice/infrastructure/clients/product/feign/dtos/DecreaseAllProductStockRequest.java
+++ order-service/src/main/java/on/logistics/orderservice/infrastructure/clients/product/feign/dtos/DecreaseAllProductStockRequest.java
@@ -1,17 +1,38 @@
 package on.logistics.orderservice.infrastructure.clients.product.feign.dtos;
 
 import java.util.List;
+import java.util.UUID;
 import on.logistics.orderservice.infrastructure.clients.product.dtos.DecreaseAllProductStockRequestDto;
 
 public record DecreaseAllProductStockRequest(
-    List<DecreaseProductStockRequest> allProducts
+    List<DecreaseProductStockRequest> products
 ) {
 
+    public record DecreaseProductStockRequest(
+        UUID productId,
+        Long decreaseQuantity
+    ) {
+
+        public static DecreaseProductStockRequest of(
+            UUID productId,
+            Long decreaseQuantity
+        ) {
+            return new DecreaseProductStockRequest(
+                productId,
+                decreaseQuantity
+            );
+        }
+    }
+
     public static DecreaseAllProductStockRequest from(
-        DecreaseAllProductStockRequestDto requestDto) {
+        DecreaseAllProductStockRequestDto requestDto
+    ) {
         return new DecreaseAllProductStockRequest(
             requestDto.allProducts().stream()
-                .map(DecreaseProductStockRequest::from)
+                .map(product -> DecreaseProductStockRequest.of(
+                    product.productId(),
+                    product.quantity()
+                ))
                 .toList()
         );
     }
