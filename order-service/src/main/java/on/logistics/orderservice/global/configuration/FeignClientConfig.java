6dc4894929b5e8c951bdb06c3e8adad002e0caca
jeonjuho <wjswngh23@daum.net>
2025-04-01 20:55:32 +0900

    feat: open feign 설정 변경
    - 재시도 시도를 하도록 횟수 및 간격 설정 추가
    - 에러 응답 시 내부 예외가 발생되는 설정 추가

--- order-service/src/main/java/on/logistics/orderservice/global/configuration/FeignClientConfig.java
+++ order-service/src/main/java/on/logistics/orderservice/global/configuration/FeignClientConfig.java
@@ -1,16 +1,36 @@
 package on.logistics.orderservice.global.configuration;
 
+
 import feign.RequestInterceptor;
+import feign.Retryer;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.concurrent.TimeUnit;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignFormatterRegistrar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Configuration
 @EnableFeignClients(basePackages = "on.logistics.orderservice.infrastructure.clients")
 public class FeignClientConfig {
+
+    @Bean
+    public Retryer.Default feignRetryer() {
+        // 0.1초 간격으로 시작해 최대 3초의 간격으로 점점 증가하며, 최대 5번 재시도한다.
+        return new Retryer.Default(100L, TimeUnit.SECONDS.toMillis(3L), 5);
+    }
+
+    @Bean
+    public FeignFormatterRegistrar dateTimeFormatterRegistrar() {
+        return registry -> {
+            DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+            registrar.setUseIsoFormat(true);
+            registrar.registerFormatters(registry);
+        };
+    }
 
     @Bean
     public RequestInterceptor feignRequestInterceptor() {
